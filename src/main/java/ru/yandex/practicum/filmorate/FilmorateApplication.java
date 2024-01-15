5b8b0fae4261051e122aa4c11f28a20aa56c8976
Evgeniy9005 <evgeniy_90_05@mail.ru>
2024-01-15 22:33:31 +0300

    100% Pull request содержит двоичные файлы. Удалите их (*.class, *.jar, *.DS_Store ...) из PR

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FilmorateApplication {
